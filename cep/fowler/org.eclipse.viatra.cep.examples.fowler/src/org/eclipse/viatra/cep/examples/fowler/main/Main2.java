6c7b3ba6ae60ec9aa115d7308be54abbe33ee4cf
Istvan David <davidi@inf.mit.bme.hu>
2015-05-21 14:45:03 +0200

    [467850] Updated example with parameter retrieval
    Change-Id: I7ff1fef68d583c5312f4b0124bb4476cd780e01d
    Signed-off-by: Istvan David <davidi@inf.mit.bme.hu>

--- cep/fowler/org.eclipse.viatra.cep.examples.fowler/src/org/eclipse/viatra/cep/examples/fowler/main/Main2.java
+++ cep/fowler/org.eclipse.viatra.cep.examples.fowler/src/org/eclipse/viatra/cep/examples/fowler/main/Main2.java
@@ -13,52 +13,58 @@ import org.junit.Test;
 
 public class Main2 extends DefaultApplication {
 
-	/**
-	 * The {@link DefaultRealm} is required to run the JUnit plug-in tests
-	 * seamlessly. For details, see
-	 * https://wiki.eclipse.org/JFace_Data_Binding/Realm.
-	 */
-	private DefaultRealm defaultRealm;
+    /**
+     * The {@link DefaultRealm} is required to run the JUnit plug-in tests seamlessly. For details, see
+     * https://wiki.eclipse.org/JFace_Data_Binding/Realm.
+     */
+    private DefaultRealm defaultRealm;
 
-	public Main2() {
-		super(EventContext.CHRONICLE);
-	}
+    public Main2() {
+        super(EventContext.CHRONICLE);
+    }
 
-	@Before
-	public void setUp() {
-		defaultRealm = new DefaultRealm();
-	}
+    @Before
+    public void setUp() {
+        defaultRealm = new DefaultRealm();
+    }
 
-	@After
-	public void tearDown() {
-		defaultRealm.dispose();
-	}
+    @After
+    public void tearDown() {
+        defaultRealm.dispose();
+    }
 
-	@Test
-	public void test() throws InterruptedException {
-		getEngine().setCepEngineDebugLevel(Level.DEBUG);
+    @Test
+    public void test() throws InterruptedException {
+        getEngine().setCepEngineDebugLevel(Level.DEBUG);
 
-		getEngine().getLogger().info("staring");
-		// Pushing events to the event stream which will deliver them to the
-		// engine.
+        getEngine().getLogger().info("staring");
+        // Pushing events to the event stream which will deliver them to the engine.
 
-		DWOP_2_Event dwop_Event = CepFactory.getInstance().createDWOP_2_Event();
-		dwop_Event.setId("drawer#1");
-		DWCL_2_Event dwcl_Event = CepFactory.getInstance().createDWCL_2_Event();
-		dwcl_Event.setId("drawer#1");
+        DWOP_2_Event dwop_Event_1 = CepFactory.getInstance().createDWOP_2_Event();
+        dwop_Event_1.setId("drawer#1");
+        getEventStream().push(dwop_Event_1);
+        Thread.sleep(1000l);
 
-		getEventStream().push(dwop_Event);
-		getEventStream().push(dwcl_Event);
-		getEventStream().push(dwop_Event);
-		Thread.sleep(1000l);
-		getEventStream().push(dwcl_Event);
+        DWCL_2_Event dwcl_Event_1 = CepFactory.getInstance().createDWCL_2_Event();
+        dwcl_Event_1.setId("drawer#1");
+        getEventStream().push(dwcl_Event_1);
+        Thread.sleep(1000l);
 
-		// Some debug information about the end of the process.
-		getEngine().getLogger().info("ending");
-	}
+        DWOP_2_Event dwop_Event_2 = CepFactory.getInstance().createDWOP_2_Event();
+        dwop_Event_2.setId("drawer#1");
+        getEventStream().push(dwop_Event_2);
+        Thread.sleep(1000l);
 
-	@Override
-	protected void configureRules() {
-		getEngine().addRule(CepFactory.getInstance().createOpenSC3_2());
-	}
+        DWCL_2_Event dwcl_Event_2 = CepFactory.getInstance().createDWCL_2_Event();
+        dwcl_Event_2.setId("drawer#1");
+        getEventStream().push(dwcl_Event_2);
+
+        // Some debug information about the end of the process.
+        getEngine().getLogger().info("ending");
+    }
+
+    @Override
+    protected void configureRules() {
+        getEngine().addRule(CepFactory.getInstance().createOpenSC3_2());
+    }
 }
