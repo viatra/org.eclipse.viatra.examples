8a3c05c92d203ea4b620209462c4fffc9351baa1
Abel Hegedus <abel.hegedus@incquerylabs.com>
2016-09-22 16:01:43 +0200

    [497889][CPS] added whitespace validation rule

---
+++ cps/addons/org.eclipse.viatra.examples.cps.queries.validation/src-gen/org/eclipse/viatra/examples/cps/model/validation/IdContainsWhitespaceConstraint0.java
@@ -0,0 +1,91 @@
+/**
+
+  Copyright (c) 2014-2016 IncQuery Labs Ltd.
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  which accompanies this distribution, and is available at
+  http://www.eclipse.org/legal/epl-v10.html
+ 
+  Contributors:
+      Akos Horvath, Abel Hegedus, Akos Menyhert, Zoltan Ujhelyi - initial API and implementation
+*/
+package org.eclipse.viatra.examples.cps.model.validation;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import org.eclipse.viatra.addon.validation.core.api.Severity;
+import org.eclipse.viatra.addon.validation.core.api.IConstraintSpecification;
+import org.eclipse.viatra.query.runtime.api.IPatternMatch;
+import org.eclipse.viatra.query.runtime.api.IQuerySpecification;
+import org.eclipse.viatra.query.runtime.api.ViatraQueryMatcher;
+import org.eclipse.viatra.query.runtime.exception.ViatraQueryException;
+
+import org.eclipse.viatra.examples.cps.model.validation.util.IdContainsWhitespaceQuerySpecification;
+
+public class IdContainsWhitespaceConstraint0 implements IConstraintSpecification {
+
+    private IdContainsWhitespaceQuerySpecification querySpecification;
+
+    public IdContainsWhitespaceConstraint0() throws ViatraQueryException {
+        querySpecification = IdContainsWhitespaceQuerySpecification.instance();
+    }
+
+    @Override
+    public String getMessageFormat() {
+        return "The ID of $identifiable.identifier$ contains whitespace";
+    }
+
+
+    @Override
+    public Map<String,Object> getKeyObjects(IPatternMatch signature) {
+        Map<String,Object> map = ImmutableMap.of(
+            "identifiable",signature.get("identifiable")
+        );
+        return map;
+    }
+
+    @Override
+    public List<String> getKeyNames() {
+        List<String> keyNames = ImmutableList.of(
+            "identifiable"
+        );
+        return keyNames;
+    }
+
+    @Override
+    public List<String> getPropertyNames() {
+        List<String> propertyNames = ImmutableList.of(
+        );
+        return propertyNames;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricPropertyNames() {
+        Set<List<String>> symmetricPropertyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricPropertyNamesSet;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricKeyNames() {
+        Set<List<String>> symmetricKeyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricKeyNamesSet;
+    }
+
+    @Override
+    public Severity getSeverity() {
+        return Severity.ERROR;
+    }
+
+    @Override
+    public IQuerySpecification<? extends ViatraQueryMatcher<? extends IPatternMatch>> getQuerySpecification() {
+        return querySpecification;
+    }
+
+}
